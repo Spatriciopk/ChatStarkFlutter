cf6e6a3cf821760a97bd30dbeae266353855b5ed
Spatriciopk <104325319+Spatriciopk@users.noreply.github.com>
2023-02-09 13:18:41 -0500

    Seccion de chat terminado diseño

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
-      initialRoute: 'login', //loading es el q se sabe si esta autentificado
+      initialRoute: 'chat', //loading es el q se sabe si esta autentificado
       routes: appRoutes,
 
     );
